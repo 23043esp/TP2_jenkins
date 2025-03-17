90f42ad3815210c188422f8862e200f2b08fcbad
23043esp <23043@esp.mr>
2025-03-17 10:49:56 +0100

    2eme modification  de ficher HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins!");
         System.out.println("Hello, Nessibe!");
+        System.out.println("Hello, github!");
     }
 }
